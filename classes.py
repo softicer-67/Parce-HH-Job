84453d7edf5f9b6674f70877050555e356d4754a
Yuri <57116431+softicer-67@users.noreply.github.com>
2022-09-29 09:28:37 +0500

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,61 +1,92 @@
 import json
 from abc import ABC, abstractmethod
 import requests
+from bs4 import BeautifulSoup as bs
 
 
 class Engine(ABC):
+
     @abstractmethod
     def get_request(self):
         pass
 
 
 class HH(Engine):
-    def __init__(self, options=None, pages=0):
+    def get_request(self, options=None, pages=0):
         self.options = options
         self.pages = pages
+        super().get_request()
+        hh_data = {}
+        for page in range(self.pages):
+            with open('hh_data.json', 'w', encoding='utf-8') as f:
+                json.dump(hh_data, f, indent=4, ensure_ascii=False)
+            try:
+                current_data = json.loads(
+                    requests.get(f"https://api.hh.ru/vacancies?text={self.options}&per_page={page}").text)
+                print(f'[+] Парсим hh.ru Please wait ... {page + 1}')
+
+                for i in range(page):
+                    for item in current_data['items']:
+                        hh_data[item['id']] = {
+                            'title': item['name'].lower(),
+                            'url': item['alternate_url'],
+                            'salary': item['salary'],
+                            'description': str(item['snippet']['requirement']).replace('<highlighttext>', '').replace(
+                                '</highlighttext>', ''),
+                            'date': item['published_at']
+                        }
+                with open('hh_data.json', 'w', encoding='utf-8') as f:
+                    json.dump(hh_data, f, indent=4, ensure_ascii=False)
+            except KeyError:
+                print(f'[+] Error')
+                break
+        return hh_data
 
     @staticmethod
     def load_file():
         res = []
-        with open('all_data.json', 'r', encoding='utf-8') as f:
+        with open('hh_data.json', 'r', encoding='utf-8') as f:
             top_10 = json.load(f)
         for k, v in top_10.items():
             try:
                 date = v['date'][:10]
                 date = f'{date[-2:]}-{date[5:7]}-{date[:4]}'
                 res.append(
-                    f"{date} {v['title']} от {v['salary']['from']} до {v['salary']['to']} руб. {v['url']} {v['description'][:50]}...")
+                    f"{date} {v['title']} от {v['salary']['from']} до "
+                    f"{v['salary']['to']} руб. {v['url']} {v['description'][:50]}...")
             except TypeError:
                 continue
         return '\n'.join(res)
 
-    def get_request(self):
-        filter_data = {}
-        for page in range(self.pages):
-            try:
-                current_data = json.loads(requests.get(f"https://api.hh.ru/vacancies?text={self.options}&per_page={page+1}").text)
-                print(f'[+] Please wait ... str. {page + 1}')
-                for i in range(page):
-                    for item in current_data['items']:
-                        filter_data[item['id']] = {
-                            'title': item['name'].lower(),
-                            'url': item['alternate_url'],
-                            'salary': item['salary'],
-                            'description': str(item['snippet']['requirement']).replace('<highlighttext>', '').replace('</highlighttext>', ''),
-                            'date': item['published_at']
-                        }
-                with open('all_data.json', 'w', encoding='utf-8') as f:
-                    json.dump(filter_data, f, indent=4, ensure_ascii=False)
-            except KeyError:
-                print(f'[+] Error')
-                break
-        with open('all_data.json', 'w', encoding='utf-8') as f:
-            json.dump(filter_data, f, indent=4, ensure_ascii=False)
-        return filter_data
-
 
 class Superjob(Engine):
     def get_request(self):
-        pass
+        super().get_request()
 
+    @staticmethod
+    def load_file_2(options, pages):
+        super_data = {}
+        for page in range(pages):
+            http = 'https://www.superjob.ru'
+            url = f'https://russia.superjob.ru/vacancy/search/?keywords={options}&page={page}'
+            print(f'[+] Парсим superjob.ru Please wait ... {page + 1}')
+            response = requests.get(url)
+            soup = bs(response.text, 'lxml')
+            title = soup.find_all('span', class_='_9fIP1 _249GZ _1jb_5 QLdOc')
+            salary = soup.find_all('span', class_='_2eYAG _1nqY_ _249GZ _1jb_5 _1dIgi')
+            desc = soup.find_all('span', class_='_1Nj4W _249GZ _1jb_5 _1dIgi _3qTky')
+            for i in range(pages):
+                try:
+                    super_data[i] = {
+                            'title': title[i].text,
+                            'salary': salary[i].text.replace('\xa0', ' '),
+                            'url': http + title[i].a['href'],
+                            'desc': desc[i].text
+                        }
+                    with open('super_data.json', 'w', encoding='utf-8') as f:
+                        json.dump(super_data, f, indent=4, ensure_ascii=False)
+                except IndexError:
+                    continue
+
+        return super_data
 
--- main.py
+++ main.py
@@ -1,32 +1,50 @@
-from classes import HH
+from classes import HH, Superjob
 
 
 def main():
+    global opt_1, file
     while True:
         try:
-            option = input('Введите слово для поиска вакансии: ')
-            pages = int(input('Колличество вакансий: '))
-            opt = HH(option, pages)
-            opt.get_request()
-        except ValueError:
-            print('Ошибка ввода')
-            continue
-        while True:
-            file = opt.load_file()
             res = []
-            print(f'\n[1] - Вывод на экран:\n[2] - Сортировка по дате:\n[3] - Сохранить в json и выйти')
-            user_input = input('Что посмотрим?: \n')
+            res_2 = []
+            print(f'\n[1] - Парсить hh.ru:\n[2] - Вывод на экран:\n'
+                  f'[3] - Сортировка по дате:\n\n[4] - Парсить superjob.ru:\n'
+                  f'[5] - Вывод на экран:\n\n[6] - Сохранить в json и выйти')
+            user_input = input('\nЧто посмотрим?: \n')
             if user_input == '1':
-                print(opt.load_file())
+                option = input('Введите слово для поиска вакансии: ')
+                pages = int(input('Колличество вакансий: '))
+                opt_1 = HH()
+                opt_1.get_request(option, pages)
             elif user_input == '2':
+                file = opt_1.load_file()
+                print(file)
+            elif user_input == '3':
                res.append(file.rstrip('\n'))
                res = '\n'.join(res)
                print('\n'.join(sorted(res.split('\n'), reverse=True)))
-            elif user_input == '3':
+
+            elif user_input == '4':
+                option = input('Введите слово для поиска вакансии: ')
+                pages = int(input('Колличество вакансий: \n'))
+                opt_2 = Superjob()
+                file = opt_2.load_file_2(option, pages)
+            elif user_input == '5':
+                for k, v in file.items():
+                    try:
+                        res_2.append(
+                            f"{v['title']} от {v['salary']} руб {v['url']} {v['desc'][:50]}...")
+                    except TypeError:
+                        continue
+                res_2 = str(res_2).rstrip('\n')
+                print('\n'.join(res_2.split(', ')).replace('[', '').replace(']', '').replace("'", ''))
+
+            elif user_input == '6':
                 print('Выход')
                 break
+        except Exception:
             continue
-        exit('Досвидания')
+    exit('Досвидания')
 
 
 if __name__ == '__main__':
