0c44f35ee97d483fb5c73c00f1b0c4748c71a20e
Yuri <57116431+softicer-67@users.noreply.github.com>
2022-09-30 23:06:33 +0500

    Add files via upload
    Убрал лишние запросы

--- main.py
+++ main.py
@@ -2,45 +2,43 @@ from utils import *
 
 
 def main() -> None:
+    data = None
     while True:
         user_input = input(f'\n[1] - Парсить hh.ru\n[2] - Вывести все\n'
                            f'[3] - Сортировать по дате\n[4] - Вывести 10 последних\n\n'
-                           f'[5] - Парсить superjob.ru\n[6] - Вывести все\n[7] - Вывести 10 последних\n\n'
-                           f'[8] - Сохранить в json и выйти\n')
+                           f'[5] - Парсить superjob.ru\n[6] - Вывести все\n[7] - Сортировать по дате\n'
+                           f'[8] - Вывести 10 последних\n\n[9] - Сохранить в json и выйти\n')
+
         match user_input:
             case '1':
-                option = input('Введите слово для поиска вакансии: ')
+                option = input('Введите слово для поиска вакансии hh: ')
                 pages = int(input('Колличество вакансий: '))
                 get_hh_vacs(option, int(pages))
+                data = output_hh()
             case '2':
-                data = output_hh()
                 print('\n'.join(data))
-                print('-' * 150)
             case '3':
-                data = output_hh()
                 res = '\n'.join(data)
                 print('\n'.join(sorted(res.split('\n'), reverse=True)))
-                print('-' * 150)
             case '4':
-                data = output_hh()
                 res = '\n'.join(data)
                 res = '\n'.join(set(str(res).split('\n')))
                 print('\n'.join(sorted(res.split('\n')[:10], reverse=True)))
-                print('-' * 150)
             case '5':
-                option = input('Введите слово для поиска вакансии: ')
+                option = input('Введите слово для поиска вакансии sj: ')
                 pages = int(input('Колличество вакансий: '))
                 get_sj_vacs(option, int(pages))
+                data = output_sj()
             case '6':
-                data = output_sj()
                 print('\n'.join(data))
-                print('-' * 150)
             case '7':
-                data = output_sj()
+                res = '\n'.join(data)
+                res = '\n'.join(set(str(res).split('\n')))
+                print('\n'.join(sorted(res.split('\n'), reverse=True)))
+            case '8':
                 res = '\n'.join(data)
                 res = '\n'.join(set(str(res).split('\n')))
                 print('\n'.join(sorted(res.split('\n')[:10], reverse=True)))
-                print('-' * 150)
             case _:
                 break
 
